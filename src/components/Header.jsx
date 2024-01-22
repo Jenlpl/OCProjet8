498c173641eebf30a1705df36b329806d0ccbf2c
Evgeniya Plumey <e.plumey@outlook.com>
2024-01-22 16:05:37 +0100

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+    return (
+        <nav className="navbar">
+      <div className="logo">
+        <img src="src/assets/images/logo.png" alt="" ></img>
+      </div>
+      <ul>
+        <li>
+            <a href="">Accueil</a>
+            </li>
+            <li>
+            <a href="">A propos</a>
+            </li>
+      </ul>
+      </nav>
+    )
+}
+
