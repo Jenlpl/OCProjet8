4d17da99f980cfbdf714c1b036512d66d98eab38
Evgeniya Plumey <e.plumey@outlook.com>
2024-01-24 20:48:33 +0100

    Collapse

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,16 @@
+
+export default function Collapse() {
+    return (
+        <div className="collapse-wrapper">
+            <div className="collapse">
+                <h2>Fiabilité</h2></div>
+                {/* <FontAwesomeIcon icon="fa-solid fa-chevron-up" /> */}
+            <div className="collapse">
+            <h2>Respect</h2></div>
+            <div className="collapse">
+            <h2>Service</h2></div>
+            <div className="collapse">
+            <h2>Sécurité</h2></div>
+        </div>
+    )
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import Collapse from "../components/Collapse";
 
 export default function About() {
     return (
         <main className="about">
+          <Collapse />
       </main>
     )
 }
